1c7a73bcc4ec0a22973b23ab6651e7777ed5c154
malgueroc@gmail.com <malgueroc@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-07 14:47:25 +0000

    Inventory : ajout de la classe ItemService2 qui fusionnera avec ItemService à terme.

---
+++ Inventory/src/javablackbelt/inventory/itemservice/ItemService2.java
@@ -0,0 +1,52 @@
+package javablackbelt.inventory.itemservice;
+
+import javablackbelt.inventory.model.Group;
+import javablackbelt.inventory.model.Item;
+import javablackbelt.inventory.model.User;
+
+
+
+public class ItemService2 {
+
+	
+	public void sendItemTo(User sender,Item item, User receiver){
+		//ajouter l'item dans la InventoryItems de la cible
+		receiver.getInventoryItems().add(item);
+		
+		//changer l'owner de l'item
+		item.setOwner(receiver);
+		
+		//envoi d'un email au receveur
+		System.out.println(receiver.getNickName()+ " vient de recevoir dans son inventaire "
+				+item.getItemType().getItemName()+ " de la part de "+sender.getNickName());
+		
+		//supprimer l'item de la InventoryItems de l'envoyeur
+		sender.getInventoryItems().remove(item);
+	}
+	
+	
+	
+	public void activateItemTo(User sender,Item item, User receiver){
+		//afficher sur la page l'objet envoyé
+		System.out.println(item.getItemType().getItemName()
+				+ " vient d'être activé sur la page de "+receiver.getNickName()+" !");
+		
+		//ajouter l'item dans la InventoryItems de la cible
+		receiver.getInventoryItems().add(item);
+		
+		//initialiser la activationDate et calculer la removalDate
+		item.setActivationDate();
+		
+		//initialiser la variable activationDescription (Example : "Refreshing Pint of Belgian Beer offered by (sender)".
+		
+		
+		//changer l'owner de l'item
+		item.setOwner(receiver);
+		
+		//supprimer l'item de la InventoryItems de l'envoyeur
+		sender.getInventoryItems().remove(item);
+	}
+	
+	
+	
+}
--- Inventory/src/javablackbelt/inventory/test/main/Main.java
+++ Inventory/src/javablackbelt/inventory/test/main/Main.java
@@ -1,6 +1,9 @@
 package javablackbelt.inventory.test.main;
 
-import javax.swing.JOptionPane;
+import javablackbelt.inventory.itemservice.ItemService2;
+import javablackbelt.inventory.model.*;
+
+
 
 public class Main {
 
@@ -9,13 +12,32 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
+		User pierre = new User("Pierre");
+		User david = new User("David");
+		User mat = new User ("Mat");
 		
-		String response = JOptionPane.showInputDialog(null,
-				  "What is the target's nickname ?",
-				  "Enter the target's nickname",
-				  JOptionPane.QUESTION_MESSAGE);
+		Group g1=new Group("Bru-JUG");
 		
-		System.out.println(response);
+		
+		
+		
+		Item beerGlass = new Item(pierre,ItemType.BEER_PINT);
+		Item back_sleep = new Item(pierre,ItemType.BAKG_SLEEP);
+		
+		// à mon avis, pas top. Ce n'est pas à l'utilisateur d'ajouter un objet
+		// à sa liste mais à la classe ItemService. Pour les tests, on utilise le user.addItem(item);
+		pierre.addItem(beerGlass); 
+		pierre.addItem(back_sleep);
+		
+		ItemService2 iService = new ItemService2();
+		
+		
+		iService.sendItemTo(pierre,beerGlass,david);
+	
+		//gerer les receveurs : USER, GROUP, GLOBAL
+		
+		
+		
 	}
 
 	
