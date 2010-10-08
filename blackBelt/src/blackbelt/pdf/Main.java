eff14c7f6b1552be8a053d45d2a83759533e9a1a
julien.vas@gmail.com <julien.vas@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-08 10:14:10 +0000

    BlackBeltPdf
    Mise à jour du pdf generator

--- blackBelt/src/blackbelt/pdf/Main.java
+++ blackBelt/src/blackbelt/pdf/Main.java
@@ -1,12 +1,12 @@
 package blackbelt.pdf;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.itextpdf.text.DocumentException;
 
 public class Main {
 
-	public static void main(String[] args) throws DocumentException, FileNotFoundException {
+	public static void main(String[] args) throws DocumentException, IOException {
 		
 		TestDataGenerator dataGenerator = new TestDataGenerator();
 		dataGenerator.generate();
