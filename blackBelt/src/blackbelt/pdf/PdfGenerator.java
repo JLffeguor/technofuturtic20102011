6853ec5b0a858b3a452b0d61f7e1d5edc998bb81
julien.vas@gmail.com <julien.vas@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-08 10:14:33 +0000

    BlackBeltPdf
    Toujours pdf Generator

--- blackBelt/src/blackbelt/pdf/PdfGenerator.java
+++ blackBelt/src/blackbelt/pdf/PdfGenerator.java
@@ -1,26 +1,31 @@
 package blackbelt.pdf;
 
-
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
 
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.html.simpleparser.StyleSheet;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import coursepage.CourseTextFormatter;
 
-/** JE SERT A KOI ?????? */
+/** This class generate a PDF document */
 public class PdfGenerator {
 	
-	private static final String DOCUMENT_RESULT = ("C:\\Users\\forma704\\Documents\\Julien\\blackBelt\\HtmlPage.pdf");
+	private static final String DOCUMENT_RESULT = ("S:\\DocumentsPourPDF\\HtmlPage.pdf");
 	private Document doc = new Document();
 	private boolean isUse = false;
 
 	
-	public void generatePdf(Section rootSection) throws FileNotFoundException, DocumentException {
-				// CA FAIT KOI??
+	public void generatePdf(Section rootSection) throws DocumentException, IOException {
+		//Generate the PDF document
 		PdfWriter.getInstance(this.doc, new FileOutputStream(DOCUMENT_RESULT));
 		
 		if (!this.isUse){
@@ -34,15 +39,28 @@ public class PdfGenerator {
 	}
 
 
-	/** Accumulates paragraphs for the sections (and its subsections) in this.doc */
-	private void recursiveWalk(Section currentSection) throws DocumentException, FileNotFoundException{
+	/** Accumulates paragraphs for the sections (and its subsections) in this.doc 
+	 * @throws IOException */
+	private void recursiveWalk(Section currentSection) throws DocumentException, IOException{
 		// Title
-		this.doc.add(new Paragraph(currentSection.getTitle()));
+			//Style
+		Font fontTitle = new Font(Font.getFamily(DOCUMENT_RESULT), 18, Font.BOLD);
+		fontTitle.setSize(18);
+		fontTitle.setColor(170, 0, 0);
+			//Text and Style
+		Chunk pTitle = new Chunk(currentSection.getTitle(), fontTitle);
+			//Add Text
+		this.doc.add(pTitle);
 		
 		// Body
-		CourseTextFormatter sectionFormatter = new CourseTextFormatter("",
-				currentSection.getBody());
-		this.doc.add(new Paragraph(sectionFormatter.format()));
+		CourseTextFormatter sectionFormatter = new CourseTextFormatter("", currentSection.getBody());
+		StyleSheet styleBody = new StyleSheet();
+		styleBody.loadStyle("p", "size", "12px"); //Style of the body
+		List<Element> objBody; //Create List with the paragraph
+		objBody = HTMLWorker.parseToList(new StringReader(sectionFormatter.format()), styleBody); //Format text and apply style
+			for (int i = 0; i < objBody.size(); i++){
+				this.doc.add(objBody.get(i)); //Add paragraph
+			}
 
 		// Childs (recursive call)
 		for (Section sSection : currentSection.getSubSections()) {
