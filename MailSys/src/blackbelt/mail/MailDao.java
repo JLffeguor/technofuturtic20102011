42b65a0269c5613d5b5dffd23db97f00d9617ae4
javablackbeltproject <javablackbeltproject@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-08 12:41:13 +0000

    MailSys:
    Commit d'un premier jet sur le retrait des mails de la db.
    Ceci ne constitue pas un code final mais un code permettant de ne pas bloquer les développements.

--- MailSys/src/blackbelt/mail/MailDao.java
+++ MailSys/src/blackbelt/mail/MailDao.java
@@ -2,32 +2,64 @@
 package blackbelt.mail;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import blackbelt.model.Mail;
+import blackbelt.model.User;
 
-/** Fake Dao to be replace by JPA code */	
+/** Fake Dao to be replace by JPA code */
 public class MailDao {
 
-	public static MailDao instance = new MailDao();  // TODO: replace by being a Sping bean. 
+	final static int MAXEMAILFORASEND = 2;
 	
-	ArrayList<Mail> db ;
-	ArrayList<Mail> stendDb ;
-	public MailDao(){
-		 db = new ArrayList<Mail>();
+	public static MailDao instance = new MailDao(); // TODO: replace by being a
+													// Sping bean.
+
+	ArrayList<Mail> db;
+	ArrayList<Mail> stendDb;
+
+	public MailDao() {
+		db = new ArrayList<Mail>();
 	}
-	
-	
-	public void save(Mail mail){
-			db.add(mail);
+
+	public void save(Mail mail) {
+		db.add(mail);
 	}
-	public ArrayList<Mail> getDb(){
+
+	public ArrayList<Mail> getDb() {
 		return this.db;
 	}
-	public Mail findNextMail(){
-		Mail nextMail = (this.db.size()>0)
-					?
-				this.db.remove(0):null;
-		return nextMail;
+
+	public List<Mail> findNextMail() {
+
+		int i, max;
+		List<Mail> rc;
+		Mail currentMail;
+		User firstUser;
+
+		if (this.db.size() == 0) {
+			return null;
+		}
+
+		firstUser = this.db.get(0).getUser();
+		rc = new ArrayList<Mail>();
+
+		for (i = 0, max = this.db.size(); i < max; i++) {
+			
+			currentMail = this.db.get(i);
+			
+			if (currentMail.getUser() == firstUser) {
+				rc.add(currentMail);
+			}
+		}
+
+		return rc;
 	}
 	
+	public void removeMails (List<Mail> mails) {
+		for (Mail mail : mails) {
+			this.db.remove(mail);
+		}
+	}
+
 }
