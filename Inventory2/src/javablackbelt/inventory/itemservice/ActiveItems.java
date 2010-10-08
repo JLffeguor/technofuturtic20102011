580881cb358658838509d891f840d6ba7a491711
nachmanmathieu@gmail.com <nachmanmathieu@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-08 14:37:29 +0000

    Inventory - ActiveItems - methods

--- Inventory2/src/javablackbelt/inventory/itemservice/ActiveItems.java
+++ Inventory2/src/javablackbelt/inventory/itemservice/ActiveItems.java
@@ -1,6 +1,7 @@
 package javablackbelt.inventory.itemservice;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,27 +11,48 @@ import javablackbelt.inventory.model.ItemType;
 import javablackbelt.inventory.model.User;
 
 public class ActiveItems {
-	
 
 	private Map<Long, List<Item>> userListActiveItemsMap = new HashMap<Long, List<Item>>();
 	private Map<Long, List<Item>> groupActiveItemsMap = new HashMap<Long, List<Item>>();
-	private List<Item> globalItems = new ArrayList<Item>();	
-	 
-    private static ActiveItems instance = new ActiveItems();  // Singleton.
-    
-    
-    public void getActiveItems(ItemType.Group itemTypeGroup){   	
-    }
-    
-    public void getActiveItems(User user){
-    	
-    }
-        
-    public void getGlobalActiveItems(ItemType itemType){
-    	
-    }
-    
-    public ActiveItems getInstance() {
-    	return instance;
-    }
+	private List<Item> globalItems = new ArrayList<Item>();
+
+	private static ActiveItems instance = new ActiveItems(); // Singleton.
+
+	private Date actualDate = new Date();
+
+	public void getActiveItems(ItemType.Group itemTypeGroup) {
+
+		for(Item i : globalItems) {
+
+		}
+	}
+
+	public void getActiveItems(User user) {
+
+		for (Item i : userListActiveItemsMap.get(user.getUserId())) {
+
+			if(actualDate.compareTo(i.getRemovalDate()) <= 0){
+				userListActiveItemsMap.remove(i);
+				user.removeItem(i);
+			} else
+				System.out.println(i.getItemType().getItemDescription());
+		}
+	}
+
+	public void getGlobalActiveItems(ItemType itemType) {
+
+		
+		for (Item i : globalItems) {
+
+			if(actualDate.compareTo(i.getRemovalDate()) <= 0){
+				globalItems.remove(i);
+			} else if (itemType.equals(i.getItemType())) {
+				System.out.println(i.getItemType().getItemName());
+			}
+		}
+	}
+
+	public ActiveItems getInstance() {
+		return instance;
+	}
 }
