ef98ed1b97f94aaeb0906db17fff47e9f988eddb
nachmanmathieu@gmail.com <nachmanmathieu@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-08 12:24:02 +0000

    Inventory - ActiveItems

--- Inventory2/src/javablackbelt/inventory/itemservice/ActiveItems.java
+++ Inventory2/src/javablackbelt/inventory/itemservice/ActiveItems.java
@@ -1,14 +1,38 @@
 package javablackbelt.inventory.itemservice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javablackbelt.inventory.model.Item;
+import javablackbelt.inventory.model.ItemType;
+
 public class ActiveItems {
 	
-	/*Map.....
-	Map<Long (useid), List<Item>>
-	Map<Long (groupid), List<Item>>
-	List<Item> (global items as background)
-	*/
+
+	private Map<String, List<Item>> userMap = new HashMap<String, List<Item>>();
+	private Map<String, List<Item>> groupMap = new HashMap<String, List<Item>>();
+	private List<Item> globalItems = new ArrayList<Item>();	
 	
     private static ActiveItems instance = new ActiveItems();  // Singleton.
+    
+    
+    public void getActiveItems(ItemType.Group itemTypeGroup){
+    	
+    }
+    
+    public void getActiveItems(user){
+    	
+    }
+    
+    public void getActiveItems(Group group){
+    	
+    }
+    
+    public void getGlobalActiveItems(ItemType itemType){
+    	
+    }
     
     public ActiveItems getInstance() {
     	return instance;
