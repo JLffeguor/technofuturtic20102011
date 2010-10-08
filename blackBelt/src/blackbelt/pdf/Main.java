f15034326ab9d3e1600c36ef0a069e9bb6bda39c
julien.vas@gmail.com <julien.vas@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-08 12:49:12 +0000

    BlackBeltPdf - new PdfGenerator with Title Font-Style (main)

--- blackBelt/src/blackbelt/pdf/Main.java
+++ blackBelt/src/blackbelt/pdf/Main.java
@@ -1,10 +1,12 @@
 package blackbelt.pdf;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.itextpdf.text.DocumentException;
 
 public class Main {
+	private static final String DOCUMENT_RESULT = ("S:\\DocumentsPourPDF\\HtmlPage.pdf");
 
 	public static void main(String[] args) throws DocumentException, IOException {
 		
@@ -12,7 +14,7 @@ public class Main {
 		dataGenerator.generate();
 
 		PdfGenerator pdfGen = new PdfGenerator();
-		pdfGen.generatePdf((dataGenerator.getRoot()));
+		pdfGen.generatePdf(dataGenerator.getRoot(), new FileOutputStream(DOCUMENT_RESULT));
 
 	}
 
