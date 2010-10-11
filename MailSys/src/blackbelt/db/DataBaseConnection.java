8d8d9bae1e3464a4fe272e181ddcf8fef32e839a
javablackbeltproject <javablackbeltproject@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-11 14:02:58 +0000

    MailSys: Add the database connector for MySQL 5.1 and a static class for the connection (May be change in the future).

---
+++ MailSys/src/blackbelt/db/DataBaseConnection.java
@@ -0,0 +1,30 @@
+package blackbelt.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+	
+	public static Connection conn;
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=123");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("db loaded");
+    }
+
+}
