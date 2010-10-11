4f9c086f54757cc02503d3a5520b848f235e1fba
malgueroc@gmail.com <malgueroc@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-10-11 13:47:32 +0000

    Inventory2 - ActiveItems - Comments and source review

--- Inventory2/src/javablackbelt/inventory/itemservice/ActiveItems.java
+++ Inventory2/src/javablackbelt/inventory/itemservice/ActiveItems.java
@@ -11,7 +11,6 @@ import javablackbelt.inventory.model.Item;
 import javablackbelt.inventory.model.ItemType;
 import javablackbelt.inventory.model.User;
 
-
 //
 public class ActiveItems {
 
@@ -29,14 +28,12 @@ public class ActiveItems {
 		List<Item> globalItems = new ArrayList<Item>();
 	}
 
-	
-	
-
-	
-	/** returns the active item list for a specific user. Return null if no list
-	update of the user's list of active items (suppress the non-active items thanks to the removal date)
-	*/
-	public List<Item> getActiveItems(User user) {
+	/**
+	 * returns the active item list for a specific user. Return null if no list
+	 * update of the user's list of active items (suppress the non-active items
+	 * thanks to the removal date)
+	 */
+	synchronized public List<Item> getActiveItems(User user) {
 
 		List<Item> tempItemList = new ArrayList<Item>();
 		Long mapKey = user.getUserId();
@@ -45,24 +42,38 @@ public class ActiveItems {
 		if (userListActiveItemsMap.get(mapKey) == null) {
 			return null;
 		} else {
-			List<Item> genericItemList = new ArrayList<Item>();
-			genericItemList.addAll(userListActiveItemsMap.get(mapKey));
+					
+			//List<Item> genericItemList = new ArrayList<Item>();
+			//genericItemList.addAll(userListActiveItemsMap.get(mapKey));
 
-			tempItemList = itemsStillActivated(genericItemList);
+			// get items which are still active
+			
+			tempItemList = itemsStillActivated(userListActiveItemsMap.get(mapKey));
+			userListActiveItemsMap.get(mapKey).clear(); // Wipe the list of the "mapKey user"
 
 			if (tempItemList != null) {
-				userListActiveItemsMap.get(mapKey).clear();
+				
+				// add the tempItemList into the userListActiveItemsMap on the "mapKey user" occurrence
+				
 				userListActiveItemsMap.put(mapKey, tempItemList);
+				return tempItemList;
+			} else {
+				
+				// removes the "mapKey user" from the map
+				
+				userListActiveItemsMap.remove(mapKey);
+				return null;
 			}
-			return tempItemList;
+
 		}
 	}
 
-	
-	/**returns the active item list for a specific group. Return null if no list
-	update of the group's list of active items (suppress the non-active items thanks to the removal date)
-	*/
-	public List<Item> getActiveItems(Group group) {
+	/**
+	 * returns the active item list for a specific group. Return null if no list
+	 * update of the group's list of active items (suppress the non-active items
+	 * thanks to the removal date)
+	 */
+	synchronized public List<Item> getActiveItems(Group group) {
 
 		List<Item> tempItemList = new ArrayList<Item>();
 		Long mapKey = group.getGroupId();
@@ -74,61 +85,82 @@ public class ActiveItems {
 			List<Item> genericItemList = new ArrayList<Item>();
 			genericItemList.addAll(groupActiveItemsMap.get(mapKey));
 
+			// get the list with the items which are still active
+
 			tempItemList = itemsStillActivated(genericItemList);
+			groupActiveItemsMap.get(mapKey).clear(); // Wipe the map of the "mapKey user"
 
 			if (tempItemList != null) {
-				groupActiveItemsMap.get(mapKey).clear();
-				groupActiveItemsMap.put(group.getGroupId(), tempItemList);
+
+				// add the list(tempItemList) with active items into the
+				// map(groupActiveItemsMap) on the specified key
+
+				groupActiveItemsMap.put(mapKey, tempItemList);
+				return tempItemList;
+			} else {
+
+				// delete the key associated with the list(tempItemList) if this
+				// one is null
+
+				groupActiveItemsMap.remove(mapKey);
+				return null;
 			}
+		}
+	}
+
+	/** returns the globally active item. Return null if no list */
+	synchronized public List<Item> getGlobalActiveItems(ItemType itemType) {
+
+		List<Item> tempItemList = new ArrayList<Item>();
+		tempItemList = itemsStillActivated(globalItems);
+
+		if(tempItemList == null){
+			return null;
+		}
+		else{
 			return tempItemList;
 		}
 	}
 
-	
-	
-	/** returns the globally active item. Return null if no list */
-	public List<Item> getGlobalActiveItems(ItemType itemType) {
-
-		
-		List<Item> tempItemList = new ArrayList<Item>();
-		tempItemList = itemsStillActivated(globalItems);
-		
-		return tempItemList;
-	}
-	
-	
-	
 	/** print active item on the itemTypeGroup (Background or image_home) */
-	public void getActiveItems(ItemType.Group itemTypeGroup) {
+	synchronized public void getActiveItems(ItemType.Group itemTypeGroup) {
+
+		// get the list with the items which are still active
 
 		List<Item> tempItemList = new ArrayList<Item>();
 		tempItemList = itemsStillActivated(globalItems);
 
 		for (Item i : tempItemList) {
+
+			// if the type from items in tempItemList is == to the parameter
+			// type
+
 			if (i.getItemType().getItemTypeGroup() == itemTypeGroup) {
 				System.out.println(i.getItemType().getItemDescription());
 			}
 		}
 	}
-	
-	
+
 	/** returns the items still activated */
-	public List<Item> itemsStillActivated(List<Item> genericItemList) {
-		// Remove old items (that should not be active anymore).
+	synchronized public List<Item> itemsStillActivated(
+			List<Item> genericItemList) {
+		
 		List<Item> tempItemList = new ArrayList<Item>();
 
 		for (Item i : genericItemList) {
+		
+			// if the date of items into the genericList is not exceeded 
+			
 			if (!(new Date().after(i.getRemovalDate()))) {
 				tempItemList.add(i);
 			}
 		}
-		genericItemList.clear();
 		return tempItemList;
 	}
 
-	
 	/** Don't call this directly, go through ItemService.activateItemOn... */
-	public void addItemToUserListActiveItemsMap(Item item, User user) {
+	synchronized public void addItemToUserListActiveItemsMap(Item item,
+			User user) {
 		Long userId = user.getUserId();
 		List<Item> userItemsList = groupActiveItemsMap.get(userId);
 
@@ -143,9 +175,8 @@ public class ActiveItems {
 		userItemsList.add(item);
 	}
 
-	
 	/** Don't call this directly, go through ItemService.activateItemOn... */
-	public void addItemToGroupActiveItemsMap(Item item, Group group) {
+	synchronized public void addItemToGroupActiveItemsMap(Item item, Group group) {
 		Long groupId = group.getGroupId();
 		List<Item> groupItemsList = groupActiveItemsMap.get(groupId);
 		if (groupItemsList == null) {
@@ -158,15 +189,15 @@ public class ActiveItems {
 		groupItemsList.add(item);
 	}
 
-	
 	/** Don't call this directly, go through ItemService.activateItemOn... */
-	public void addItemToGloballyActiveItems(Item item) {
-		for (Item i:globalItems){
-			if(i.getItemType().getItemTypeGroup()==item.getItemType().getItemTypeGroup()){
-				globalItems.set(index, item);
+	/** Replace active item by a new active item (background or image_home) */
+	synchronized public void addItemToGloballyActiveItems(Item item) {
+		for (Item i : globalItems) {
+			if (i.getItemType().getItemTypeGroup() == item.getItemType()
+					.getItemTypeGroup()) {
+				globalItems.set(globalItems.indexOf(i), item);
 			}
 		}
-		globalItems.add(item);
 	}
 
 	public static ActiveItems getInstance() {
