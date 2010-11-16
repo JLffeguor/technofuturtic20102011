5a9e00ecb408fe6d736d38fd8ea08cfcc1953157
dagiadaz@gmail.com <dagiadaz@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-11-16 15:05:59 +0000

    mailsys: adapt bugs top the db script(by Laurent).

--- MailSys/SQL/mailingdb.sql
+++ MailSys/SQL/mailingdb.sql
@@ -17,15 +17,15 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `mails`
 -- ----------------------------
-DROP TABLE IF EXISTS `mails`;
-CREATE TABLE `mails` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserId` int(11) NOT NULL,
-  `Subject` text NOT NULL,
-  `Content` longtext NOT NULL,
-  `IsImediateMessage` bit(1) NOT NULL,
-  `CreationDate` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS mails;
+CREATE TABLE mails (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  UserId int(11) NOT NULL,
+  Subject text NOT NULL,
+  Content longtext NOT NULL,
+  IsImediateMessage bit(1) NOT NULL,
+  CreationDate datetime NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
@@ -35,17 +35,17 @@ CREATE TABLE `mails` (
 -- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` text NOT NULL,
-  `email` text NOT NULL,
-  `lastMailSendedDate` datetime DEFAULT NULL,
-  `mailingDelai` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  pseudo text NOT NULL,
+  email text NOT NULL,
+  lastMailSendedDate datetime DEFAULT NULL,
+  mailingDelai int(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-ALTER TABLE `mails` ADD CONSTRAINT fk_mailhaveuser FOREIGN KEY (UserId) REFERENCES users(id);
+ALTER TABLE mails ADD CONSTRAINT fk_mailhaveuser FOREIGN KEY (UserId) REFERENCES users(id);
 -- ----------------------------
 -- Records of user
 -- ----------------------------
