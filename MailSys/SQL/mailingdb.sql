08714efb68962dd894617b5a633034b9bbeb2c51
javablackbeltproject <javablackbeltproject@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-11-17 15:06:49 +0000

    Mailsys: New script generator.

--- MailSys/SQL/mailingdb.sql
+++ MailSys/SQL/mailingdb.sql
@@ -1,59 +1,41 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : testing
-Source Server Version : 50149
-Source Host           : localhost:3306
-Source Database       : mailingdb
-
-Target Server Type    : MYSQL
-Target Server Version : 50149
-File Encoding         : 65001
-
-Date: 2010-10-11 14:34:11
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for `mails`
--- ----------------------------
 DROP TABLE IF EXISTS mails;
-CREATE TABLE mails (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  UserId int(11) NOT NULL,
-  Subject text NOT NULL,
-  Content longtext NOT NULL,
-  IsImediateMessage bit(1) NOT NULL,
-  CreationDate datetime NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of mails
--- ----------------------------
-
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  pseudo text NOT NULL,
-  email text NOT NULL,
-  lastMailSendedDate datetime DEFAULT NULL,
-  mailingDelai int(11) NOT NULL,
+
+DROP SEQUENCE IF EXISTS seq_mails_id;
+DROP SEQUENCE IF EXISTS seq_user_id;
+
+CREATE SEQUENCE seq_mails_id INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807;
+CREATE SEQUENCE seq_user_id  INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807;
+
+CREATE TABLE mails (
+  id                BIGINT  NOT NULL DEFAULT (nextval('seq_mails_id')),
+  UserId            BIGINT  NOT NULL,
+  Subject           TEXT    NOT NULL,
+  Content           TEXT    NOT NULL,
+  immadiate         BOOLEAN NOT NULL,
+  creationdate      TIMESTAMP WITH TIME ZONE NOT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+);
+
+CREATE TABLE users (
+  id                 BIGINT  NOT NULL DEFAULT (nextval('seq_user_id')),
+  pseudo             TEXT    NOT NULL,
+  email              TEXT    NOT NULL,
+  lastMailSendedDate TIMESTAMP WITH TIME ZONE NULL DEFAULT NULL,
+  maillingDelai       INT NOT NULL,
+  PRIMARY KEY (id)
+);
 
 ALTER TABLE mails ADD CONSTRAINT fk_mailhaveuser FOREIGN KEY (UserId) REFERENCES users(id);
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO users VALUES ('1', 'toto', 'toto@hotmail.com', null, '0');
-INSERT INTO users VALUES ('2', 'mami', 'mami@hotmail.com', null, '1');
-INSERT INTO users VALUES ('3', 'alain', 'alain@hotmail.com', null, '2');
-INSERT INTO users VALUES ('4', 'eric', 'eric@hotmail.com', null, '2');
-INSERT INTO users VALUES ('5', 'steph', 'steph@hotmail.com', null, '1');
-INSERT INTO users VALUES ('6', 'john', 'john@hotmail.com', null, '0');
-INSERT INTO users VALUES ('7', 'tati', 'tati@hotmail.com', null, '0');
+INSERT INTO users (pseudo, email, maillingDelai	)
+                  VALUES ('toto', 'toto@hotmail.com', '0'),
+                         ('mami', 'mami@hotmail.com', '1'),
+                         ('alain', 'alain@hotmail.com', '2'),
+                         ('eric', 'eric@hotmail.com', '2'),
+                         ('steph', 'steph@hotmail.com', '1'),
+                         ('john', 'john@hotmail.com', '0'),
+                         ('tati', 'tati@hotmail.com', '0');
 
