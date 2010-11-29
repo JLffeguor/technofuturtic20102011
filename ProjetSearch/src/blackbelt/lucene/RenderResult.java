dd47d96f9bdfd0155825d256ddf35a8552b997e2
jerronimo31@gmail.com <jerronimo31@gmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2010-11-29 14:17:53 +0000

    RenderResult version enable with LowerCase

--- ProjetSearch/src/blackbelt/lucene/RenderResult.java
+++ ProjetSearch/src/blackbelt/lucene/RenderResult.java
@@ -6,36 +6,40 @@ public class RenderResult {
 
 	public String extractResult(Document doc,String keyword) {
 		String textSectionFull = doc.get("text");
-		
+		textSectionFull = textSectionFull.toLowerCase();
 		int keyWordStart = textSectionFull.indexOf(keyword);
 		int keyWordEnd = textSectionFull.indexOf(keyword)+keyword.length();
 		
 		String subStrBeforeKeyword = "";
 		String subStrAfterKeyword = "";
 		//find first  /n caracter before (20 char next)
-		int startTextBefore = keyWordStart - 50;
+		int startSubStrBeforeKeyword = keyWordStart - 50;
 		if (keyWordStart==-1){
 			keyWordStart=0;
 		}
-		if (startTextBefore < 0){
-			startTextBefore = 0;
+		if (startSubStrBeforeKeyword < 0){
+			startSubStrBeforeKeyword = 0;
 			
 		}
-		subStrBeforeKeyword = textSectionFull.substring(startTextBefore,keyWordStart);
+		subStrBeforeKeyword = textSectionFull.substring(startSubStrBeforeKeyword,keyWordStart);
 		if(subStrBeforeKeyword.indexOf(" ")!= -1){
-		startTextBefore = subStrBeforeKeyword.indexOf(" ");
+		startSubStrBeforeKeyword = subStrBeforeKeyword.indexOf(" ");
 		}else{
-			startTextBefore=0;
+			startSubStrBeforeKeyword=0;
 		}
-		subStrBeforeKeyword =  subStrBeforeKeyword.substring(startTextBefore,subStrBeforeKeyword.length());
+		subStrBeforeKeyword =  subStrBeforeKeyword.substring(startSubStrBeforeKeyword,subStrBeforeKeyword.length());
 		//find first void caracter after (20 char next)
-		int endTextAfter = keyWordEnd + 50;
-		if (endTextAfter > textSectionFull.length()){
-			endTextAfter = textSectionFull.length();
+		int endSubStrAfterKeyword = keyWordEnd + 50;
+		if (endSubStrAfterKeyword > textSectionFull.length()){
+			endSubStrAfterKeyword = textSectionFull.length();
 		}
-		subStrAfterKeyword = textSectionFull.substring(keyWordEnd,endTextAfter);
-		endTextAfter = subStrAfterKeyword.lastIndexOf(" ");
-		subStrAfterKeyword = subStrAfterKeyword.substring(0,endTextAfter);
+		
+		subStrAfterKeyword = textSectionFull.substring(keyWordEnd,endSubStrAfterKeyword);
+		endSubStrAfterKeyword = subStrAfterKeyword.lastIndexOf(" ");
+		if(endSubStrAfterKeyword == -1){
+			endSubStrAfterKeyword = subStrAfterKeyword.length();
+		}
+		subStrAfterKeyword = subStrAfterKeyword.substring(0,endSubStrAfterKeyword);
 		
 		String finalResultText = subStrBeforeKeyword + " <b>" + keyword + "</b> " + subStrAfterKeyword ;
 		return finalResultText;
