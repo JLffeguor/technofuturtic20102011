e79b3e833cb9503d25bdfea7e5dee7a97cd90120
fatonalia@hotmail.com <fatonalia@hotmail.com@ca48e9ff-064c-c3f5-28a8-fd9186400b08>
2011-01-26 10:22:07 +0000

    MailSys : MailCategory

--- MailSys/src/blackbelt/model/MailCategory.java
+++ MailSys/src/blackbelt/model/MailCategory.java
@@ -1,5 +1,9 @@
 package blackbelt.model;
-
+/**
+ * 
+ * @author Alia Faton
+ * Each grouped mail has a category, we go through the categories of grouped mails to be send and we build the subject. 
+ */
 public enum MailCategory {
     CONTRIBUTION("Contribution(s)"),
     QUESTION("Question update(s)"),
